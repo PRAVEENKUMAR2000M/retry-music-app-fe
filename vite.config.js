943fdd105ec774521cb622d199f30d62dd1b6e9f
praveenkumar <praveenmech483@gmail.com>
2024-05-30 11:33:41 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   headers: {
-  //     'Cross-Origin-Embedder-Policy': 'unsafe-none',
-  //     'Cross-Origin-Opener-Policy': 'unsafe-none'
-  //   }
-  // }
-})
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'build', // Change this to 'build' to match Vercel's expectation
+  },
+});
